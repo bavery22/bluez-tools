367ec0b61b242e7d6323a32189c36bf8ef4de80f
Manuel Francisco Naranjo <naranjo.manuel@gmail.com>
2012-09-24 18:01:49 -0300

    added hcitool to the build system

--- jni/Android.mk
+++ jni/Android.mk
@@ -8,12 +8,51 @@ LOCAL_SRC_FILES := lib/uuid.c \
 	lib/sdp.c \
 	lib/hci.c
 
+LOCAL_CFLAGS:= \
+	-DVERSION=\"4.93\" \
+	-DSTORAGEDIR=\"/data/misc/bluetoothd\" \
+	-DNEED_PPOLL \
+	-D__ANDROID__ \
+	-DCONFIGDIR=\"/etc/bluetooth\" \
+	-DSERVICEDIR=\"/system/bin\" \
+	-DPLUGINDIR=\"/system/lib/bluez-plugin\" \
+	-DANDROID_SET_AID_AND_CAP \
+	-DANDROID_EXPAND_NAME \
+	-DOUIFILE=\"/data/misc/bluetoothd/ouifile\"
+
+include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := bluetoothd
+LOCAL_SRC_FILES := src/oui.c \
+	src/textfile.c
+
+LOCAL_CFLAGS:= \
+	-DVERSION=\"4.93\" \
+	-DSTORAGEDIR=\"/data/misc/bluetoothd\" \
+	-DNEED_PPOLL \
+	-D__ANDROID__ \
+	-DCONFIGDIR=\"/etc/bluetooth\" \
+	-DSERVICEDIR=\"/system/bin\" \
+	-DPLUGINDIR=\"/system/lib/bluez-plugin\" \
+	-DANDROID_SET_AID_AND_CAP \
+	-DANDROID_EXPAND_NAME \
+	-DOUIFILE=\"/data/misc/bluetoothd/ouifile\"
+
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := hcitool
 LOCAL_SRC_FILES := tools/hcitool.c
-LOCAL_STATIC_LIBARIES := bluetooth
+LOCAL_STATIC_LIBRARIES := bluetooth bluetoothd
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/src/
+
+LOCAL_CFLAGS:= \
+        -DVERSION=\"4.98\" \
+		-DSTORAGEDIR=\"/data/misc/bluetoothd\" \
+		-DNEED_PPOLL \
+		-D__ANDROID__
 
 include $(BUILD_EXECUTABLE)
