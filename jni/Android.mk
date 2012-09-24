69caadf9f1c444f99fe8a2548ed101b58c4df923
Manuel Francisco Naranjo <naranjo.manuel@gmail.com>
2012-09-24 20:03:38 -0300

    adding glib to build script

--- jni/Android.mk
+++ jni/Android.mk
@@ -44,6 +44,56 @@ include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 
+LOCAL_SRC_FILES:= \
+	glib/gdir.c \
+	glib/gerror.c \
+	glib/giochannel.c \
+	glib/gkeyfile.c \
+	glib/gmain.c \
+	glib/gmem.c \
+	glib/goption.c \
+	glib/gslice.c \
+	glib/gslist.c \
+	glib/gstring.c \
+	glib/gstrfuncs.c \
+	glib/gtimer.c \
+	glib/giounix.c \
+	glib/gmessages.c \
+	glib/gutf8.c \
+	glib/gfileutils.c \
+	glib/gconvert.c \
+	glib/gdataset.c \
+	glib/gtestutils.c \
+	glib/ghash.c \
+	glib/glist.c \
+	glib/gthread.c \
+	glib/garray.c \
+	glib/gutils.c \
+	glib/gatomic.c \
+	glib/gprintf.c \
+	glib/gpattern.c \
+	glib/guniprop.c \
+	glib/gpoll.c \
+	glib/grand.c \
+	glib/gunidecomp.c \
+	glib/gqsort.c \
+	glib/gstdio.c \
+	glib/gqueue.c \
+
+LOCAL_C_INCLUDES:= \
+	$(LOCAL_PATH)/glib/
+#	$(LOCAL_PATH)/../ \
+#	$(LOCAL_PATH)
+
+LOCAL_CFLAGS:= \
+	-DANDROID_STUB -fno-strict-aliasing
+
+LOCAL_MODULE := glib
+
+include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+
 LOCAL_MODULE := hcitool
 LOCAL_SRC_FILES := tools/hcitool.c
 LOCAL_STATIC_LIBRARIES := bluetooth bluetoothd
@@ -60,9 +110,15 @@ include $(BUILD_EXECUTABLE)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := gatttool
-LOCAL_SRC_FILES := attrib/gatttool.c
-LOCAL_STATIC_LIBRARIES := bluetooth bluetoothd
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/src/
+LOCAL_SRC_FILES := attrib/gatttool.c \
+	attrib/gatt.c \
+	attrib/att.c \
+	attrib/utils.c
+
+LOCAL_STATIC_LIBRARIES := bluetooth bluetoothd glib
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/src/ \
+	$(LOCAL_PATH)/btio/ \
+	$(LOCAL_PATH)/glib/
 
 LOCAL_CFLAGS:= \
         -DVERSION=\"4.98\" \
