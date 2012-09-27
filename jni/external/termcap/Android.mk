14fb71deff73c79bd4c5474634d89f075dc4563a
Manuel Francisco Naranjo <naranjo.manuel@gmail.com>
2012-09-27 15:34:18 -0300

    adding termcap from AOSP source

---
+++ jni/external/termcap/Android.mk
@@ -0,0 +1,23 @@
+LOCAL_PATH:= $(call my-dir)
+
+# ========================================================
+# libtermcap.a
+# ========================================================
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+	termcap.c  \
+	tparam.c  \
+	version.c
+
+LOCAL_C_INCLUDES += \
+	$(LOCAL_PATH)/..
+
+LOCAL_CFLAGS += \
+	-DHAVE_CONFIG_H
+
+LOCAL_MODULE := libtermcap
+
+LOCAL_MODULE_TAGS := eng
+
+include $(BUILD_STATIC_LIBRARY)
